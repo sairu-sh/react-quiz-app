231a37d0a7cb5b910ccfb262cc4de5f0ba9bc978
sairu-sh <tmg.sairush@gmail.com>
2024-02-17 18:39:17 +0545

    fetched the questions.json using json-server in an effect and then updated the state variable using a reducer function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import { questions } from "./data";
+
+const initialState = {
+  questions: [],
+
+  //loading error ready active  finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "dataReceived" };
+
+    default:
+      throw new Error("Action is unknown");
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    const res = fetch("http://localhost:8000/questions")
+      .then((res) => {
+        if (!res.ok) throw new Error("Error fetching the data");
+        return res.json();
+      })
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => {
+        console.log(err);
+        return questions;
+      });
+  }, []);
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,54 +1,49 @@
 import { useReducer, useState } from "react";
 
+const initialState = { count: 0, step: 1 };
 function reducer(state, action) {
   console.log(state, action);
 
   switch (action.type) {
     case "dec":
-      return { ...state, count: state.count - 1 };
+      return { ...state, count: state.count - state.step };
     case "inc":
-      return { ...state, count: state.count + 1 };
+      return { ...state, count: state.count + state.step };
     case "setCount":
       return { ...state, count: action.payload };
+    case "setStep":
+      return { ...state, step: action.payload };
+    case "reset":
+      return initialState;
     default:
       throw new Error("unknown action");
   }
 }
 function DateCounter() {
-  // const [count, setCount] = useState(0);
-  // const [step, setStep] = useState(1);
-
-  const initialState = { count: 0, step: 1 };
-  const [state, dispatchh] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
   const { count, step } = state;
   // This mutates the date object.
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    dispatchh({ type: "dec" });
-    // setCount((count) => count - 1);
-    // setCount((count) => count - step);
+    dispatch({ type: "dec" });
   };
 
   const inc = function () {
-    dispatchh({ type: "inc" });
-    // setCount((count) => count + 1);
-    // setCount((count) => count + step);
+    dispatch({ type: "inc" });
   };
 
   const defineCount = function (e) {
-    // setCount(Number(e.target.value));
-    dispatchh({ type: "setCount", payload: Number(e.target.value) });
+    dispatch({ type: "setCount", payload: Number(e.target.value) });
   };
 
   const defineStep = function (e) {
-    // dispatchh({type: 'set', payload: });
+    dispatch({ type: "setStep", payload: Number(e.target.value) });
   };
 
   const reset = function () {
-    // setCount(0);
-    // setStep(1);
+    dispatch({ type: "reset" });
   };
 
   return (
