f145f854b754793871eb15eaf6875deca462ae33
sairu-sh <tmg.sairush@gmail.com>
2024-02-17 23:31:54 +0545

    added a points state to the initial state and update it in the reducer according to the answer selected

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,8 +1,18 @@
-function Options({ question }) {
+function Options({ question, dispatch, answer }) {
+  const answerSelected = answer != null;
   return (
     <div className="options">
-      {question.options.map((option) => (
-        <button className="btn btn-option" key={option}>
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            index === question.correctOption && answerSelected
+              ? "correct"
+              : "wrong"
+          }`}
+          key={option}
+          disabled={answerSelected}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
           {option}
         </button>
       ))}
